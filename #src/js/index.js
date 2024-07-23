a700efd689aecdda913281c179bfb3bd96bde1e5
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-07-23 17:33:55 +0300

    23 July 2024

--- #src/js/index.js
+++
@@ -1,3 +0,0 @@
-
-import { piningHendle } from "./layouts/layouts";
-piningHendle();
--- #src/js/main.js
+++ #src/js/main.js
@@ -1,48 +1,10 @@
 import loaded from './modules/preloader.js';
 loaded('.preloader');
 //* ----------------------------------------------------------------------------
-// import { sidebarMenuHendle } from "./layouts/layouts.js";
-// sidebarMenuHendle();
-// -----------------------------------------------------------------------------
-import Swiper from 'swiper/bundle';
-// -----------------------------------------------------------------------------
-function bindSwipers(...swiperList) {
-	for (const swiper of swiperList) {
-		swiper.setTranslate = function (translate, byController, doNotPropagate) {
-			if (doNotPropagate) {
-				Swiper.prototype.setTranslate.apply(this, arguments);
-			} else {
-				for (const swiper of swiperList) {
-					swiper.setTranslate(translate, byController, true);
-				}
-			}
-		};
-		swiper.setTransition = function (duration, byController, doNotPropagate) {
-			if (doNotPropagate) {
-				Swiper.prototype.setTransition.apply(this, arguments);
-			} else {
-				for (const swiper of swiperList) {
-					swiper.setTransition(duration, byController, true);
-				}
-			}
-		};
-	}
-}
-
-// -----------------------------------------------------------------------------
-document.querySelectorAll('.slider').forEach((n, i) => {
-	window[`slider${i + 1}`] = new Swiper(n, {
-		freeMode: true,
-		grabCursor: true,
-		// centeredSlides: true,
-		direction: 'vertical',
-		mousewheel: true,
-		slidesPerView: 1.75,
-		slidesOffsetBefore: 15,
-		spaceBetween: 30,
-	});
-});
-bindSwipers(slider1, slider2, slider3);
-// -----------------------------------------------------------------------------
-import { sidebarMenuHendle } from "./layouts/layouts";
+import { sidebarMenuHendle } from "./layouts/layouts.js";
 sidebarMenuHendle();
+// -----------------------------------------------------------------------------
+
+
+
+
