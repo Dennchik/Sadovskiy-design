76f5514bfe9f407ea3dd8c8084e7c2d9751df731
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-09-10 16:30:46 +0300

    10 Sent 2024

--- #src/js/main.js
+++ #src/js/main.js
@@ -7,38 +7,18 @@ sidebarMenuHendle();
 import { anchorsSmoothScrolling } from "./modules/anchors-smooth-scrolling.js";
 anchorsSmoothScrolling();
 scalingFunction();
-// function () {
-// 	function resetZoom() {
-// 		const scale = 1 / (window.devicePixelRatio || 1);
-// 		document.body.style.transform = `scale(${scale})`;
-// 		document.body.style.transformOrigin = '0 0';
-// 		document.body.style.width = `${(100 / scale)}%`;
-// 	}
 
-// 	window.addEventListener('resize', resetZoom);
-// 	resetZoom(); // Dastlabki chaqirish
-// };
-// function name() {
-// 	function resetZoom() {
-// 		const scale = 1 / (window.devicePixelRatio || 1);
-// 		document.body.style.transform = `scale(${scale})`;
-// 		document.body.style.transformOrigin = '0 0';
-// 		document.body.style.width = `${(100 / scale)}%`;
-// 	}
 
-// 	window.addEventListener('resize', resetZoom);
-// 	resetZoom(); // Dastlabki chaqirish
+
+
+
+
+// function resetZoom() {
+// 	const scale = 1 / (window.devicePixelRatio || 1);
+// 	document.body.style.transform = `scale(${scale})`;
+// 	document.body.style.transformOrigin = '0 0';
+// 	document.body.style.width = `${(100 / scale)}%`;
 // }
 
-// name();
-
-
-function resetZoom() {
-	const scale = 1 / (window.devicePixelRatio || 1);
-	document.body.style.transform = `scale(${scale})`;
-	document.body.style.transformOrigin = '0 0';
-	document.body.style.width = `${(100 / scale)}%`;
-}
-
-window.addEventListener('resize', resetZoom);
-resetZoom(); // Dastlabki chaqirish
+// window.addEventListener('resize', resetZoom);
+// resetZoom(); // Dastlabki chaqirish
