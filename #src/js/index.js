98b3c4e4b7c09239fafbf3d1b7bf94667025bf5c
Jasurbek <105726004+RealPosterumAdmin@users.noreply.github.com>
2024-11-05 11:56:59 +0300

    small_edit
    Добавленно не которий проверки классов перед их оброботки

--- #src/js/index.js
+++ #src/js/index.js
@@ -3,23 +3,34 @@ import { brandsSlide } from "./modules/brands-slide.js";
 brandsSlide();
 // -----------------------------------------------------------------------------
 const contentItems = document.querySelectorAll('.services-price__column');
-contentItems.forEach(contentItem => {
 
-	const openButton = contentItem.querySelector('.card-price__open-btn');
-	const closeButton = contentItem.querySelector('.card-price__close-btn');
+if (contentItems.length > 0) {
+	contentItems.forEach(contentItem => {
+		const openButton = contentItem.querySelector('.card-price__open-btn');
+		const closeButton = contentItem.querySelector('.card-price__close-btn');
 
+		if (openButton) {
+			openButton.addEventListener('click', () => {
+				contentItem.classList.add('_open');
+			});
+		}
 
-	openButton.addEventListener('click', () => {
-		contentItem.classList.add('_open');
+		if (closeButton) {
+			closeButton.addEventListener('click', () => {
+				contentItem.classList.remove('_open');
+			});
+		}
 	});
-	closeButton.addEventListener('click', () => {
-		contentItem.classList.remove('_open');
-	});
-});
+}
 // -----------------------------------------------------------------------------
 import { opacityForEachItems } from "./animations/layout-anime.js";
-opacityForEachItems('.company-team__content', '.company-team__column');
-opacityForEachItems('.about-company__content', '.about-company__row');
+if (document.querySelector('.company-team__content')) {
+	opacityForEachItems('.company-team__content', '.company-team__column');
+}
+
+if (document.querySelector('.about-company__content')) {
+	opacityForEachItems('.about-company__content', '.about-company__row');
+}
 // -----------------------------------------------------------------------------
 
 
