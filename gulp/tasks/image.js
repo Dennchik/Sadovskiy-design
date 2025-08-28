3d6240158881f5cc7531bfc4dcf945a808d80441
Alexandr <104072970+Dennchik@users.noreply.github.com>
2025-08-28 12:00:33 +0300

    28.08.2025.v11-55

--- gulp/tasks/image.js
+++ gulp/tasks/image.js
@@ -4,39 +4,22 @@ import webp from 'gulp-webp';
 
 //* Converting and compressing images
 export function image() {
-	return $.gulp.src($.path.image.src, { encoding: false })
-		.pipe($.plumber({
-			errorHandler: $.notify.onError(error => ({
-				title: 'Image',
-				message: error.message
-			}))
-		}))
-
-		// .pipe($.newer($.path.image.dest))
-		// .pipe(webp())
-		// .pipe($.gulp.dest($.path.image.dest))
-		// .pipe($.newer($.path.image.dest))
-		// .pipe($.gulp.src($.path.image.src))
-		// .pipe($.size({ title: 'До сжатия - (Images):' }))
-		// // .pipe($.gulpIf($.app.isProd, imagemin($.app.imageMin.svgo)))
-		// // .pipe(imagemin($.app.imageMin.svgo))
-		// .pipe($.gulp.dest($.path.image.dest))
-		// .pipe($.size({ title: 'После сжатия - (Images):' }))
-		// .pipe( $.debug({ title: '(Image)' }));
-		.pipe($.newer($.path.image.dest))
-		.pipe($.gulpIf($.app.isProd, $.size({ title: 'До сжатия-Image:' })))
-		.pipe($.gulpIf($.app.isProd, imagemin($.app.imagemin)))
-		.pipe($.gulpIf($.app.isProd, $.size({ title: 'После сжатия-Image:' })))
-		.pipe($.gulp.dest($.path.image.dest))
-		.pipe(webp())
-		.pipe($.newer($.path.image.dest))
-		.pipe($.gulp.dest($.path.image.dest));
+  return $.gulp
+    .src($.path.image.src, { encoding: false })
+    .pipe(
+      $.plumber({
+        errorHandler: $.notify.onError((error) => ({
+          title: 'Image',
+          message: error.message,
+        })),
+      }),
+    )
+    .pipe($.newer($.path.image.dest))
+    .pipe($.gulpIf($.app.isProd, $.size({ title: 'До сжатия-Image:' })))
+    .pipe($.gulpIf($.app.isProd, imagemin($.app.imagemin)))
+    .pipe($.gulpIf($.app.isProd, $.size({ title: 'После сжатия-Image:' })))
+    .pipe($.gulp.dest($.path.image.dest))
+    .pipe(webp())
+    .pipe($.newer($.path.image.dest))
+    .pipe($.gulp.dest($.path.image.dest));
 }
-
-// .pipe($.gulpIf($.app.isProd, $.size({ title: 'До сжатия-Image:' })))
-// .pipe($.gulpIf($.app.isProd, imagemin($.app.imagemin)))
-// .pipe($.gulpIf($.app.isProd, $.size({ title: 'После сжатия-Image:' })))
-
-// .pipe($.size({ title: 'До сжатия - (Images):' }))
-// .pipe(imagemin($.app.imageMin.svgo))
-// .pipe($.size({ title: 'После сжатия - (Images):' }))
